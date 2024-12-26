92e645fc6420dfb1247f6cd58ac6c6e9ed099927
Oleksii Leonov <oleksii_leonov1@epam.com>
2024-12-26 22:32:52 +0200

    Added Dockerfile

--- src/main/java/com/example/app/controller/SampleRestController.java
+++ src/main/java/com/example/app/controller/SampleRestController.java
@@ -1,6 +1,7 @@
 package com.example.app.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,15 @@ import java.util.Map;
 @RestController
 public class SampleRestController {
 
+    @Value("${some.external.value:not defined}")
+    private String externalConfigValue;
+
     @GetMapping
     public Map<Object, Object> testMethod() {
         log.info("testMethod");
-        return Map.of("hello", "world", "i1", 1247, "b1", true);
+        return Map.of("external value", externalConfigValue,
+                "hello", "world",
+                "i2", 1247,
+                "b1", true);
     }
 }

4fac72d1f411ae29e7f79ef685ea2556348f1069
Oleksii Leonov <oleksii_leonov1@epam.com>
2024-12-30 17:45:17 +0200

    Change 55

--- src/main/java/com/example/app/controller/SampleRestController.java
+++ src/main/java/com/example/app/controller/SampleRestController.java
@@ -16,7 +16,7 @@ public class SampleRestController {
 
     @GetMapping
     public Map<Object, Object> testMethod() {
-        log.info("some testMethod 44");
+        log.info("some testMethod 55");
         return Map.of("external value", externalConfigValue,
                 "hello", "world",
                 "i2", 1247,

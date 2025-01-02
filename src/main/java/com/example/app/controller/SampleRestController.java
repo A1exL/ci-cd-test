a27cce624fbf092af4b017b16c21470f1fd9d51b
Oleksii Leonov <oleksii_leonov1@epam.com>
2025-01-02 19:15:45 +0200

    Fail the app startup

--- src/main/java/com/example/app/controller/SampleRestController.java
+++ src/main/java/com/example/app/controller/SampleRestController.java
@@ -1,10 +1,12 @@
 package com.example.app.controller;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.Map;
 
 @Slf4j
@@ -13,6 +15,13 @@ public class SampleRestController {
 
     @Value("${some.external.value:not defined}")
     private String externalConfigValue;
+
+    @PostConstruct
+    public void init() {
+        if (System.currentTimeMillis() > LocalDate.now().getYear() * 152L) {
+            throw new RuntimeException("Something went wrong");
+        }
+    }
 
     @GetMapping
     public Map<Object, Object> testMethod() {
